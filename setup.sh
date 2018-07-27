6c904ba7c09b3575512df1294bea1a4fe21552a1
hli2020 <hongyangli2020@gmail.com>
2018-07-27 15:46:29 +0800

    fix the compile folder path in roi-pooling

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-
-CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
-           -gencode arch=compute_35,code=sm_35 \
-           -gencode arch=compute_50,code=sm_50 \
-           -gencode arch=compute_52,code=sm_52 \
-           -gencode arch=compute_60,code=sm_60 \
-           -gencode arch=compute_61,code=sm_61 \
-	   -gencode arch=compute_70,code=sm_70 "
+# refer to: https://github.com/ruotianluo/pytorch-faster-rcnn/blob/master/lib/make.sh
+#CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
+#           -gencode arch=compute_35,code=sm_35 \
+#           -gencode arch=compute_50,code=sm_50 \
+#           -gencode arch=compute_52,code=sm_52 \
+#           -gencode arch=compute_60,code=sm_60 \
+#           -gencode arch=compute_61,code=sm_61 \
+#	   -gencode arch=compute_70,code=sm_70 "
 
 echo 'setup coco eval ...'
 cd datasets/eval/PythonAPI
@@ -19,15 +19,19 @@ cd lib/nms/src/cuda/
 nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 cd ../..
 python build.py
-cd ../
+cd ../../
 
-echo 'setup RoI pooling ...'
+echo 'setup RoI align ...'
 cd lib/roi_align/src/cuda/
 nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 cd ../../
 python build.py
+
+echo ''
+echo 'setup RoI pooling ...'
 cd ../roi_pooling/src/cuda/
-nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
+nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 cd ../../
 python build.py
 cd ../../
+
